96b5f81d8b61c11f87e92c815659dbec7b466859
Nicolas Juillerat <tachyon.sonics@gmail.com>
2025-04-18 17:35:06 +0200

    Use JPopupMenu for tray icon

--- Library/src/ch/chaos/library/Menus.java
+++ Library/src/ch/chaos/library/Menus.java
@@ -1,24 +1,33 @@
 package ch.chaos.library;
 
 import java.awt.AWTException;
+import java.awt.GraphicsConfiguration;
+import java.awt.GraphicsEnvironment;
 import java.awt.MenuItem;
 import java.awt.MenuShortcut;
 import java.awt.PopupMenu;
 import java.awt.SystemTray;
 import java.awt.TrayIcon;
+import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.geom.AffineTransform;
 import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Consumer;
 
+import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
 import javax.swing.JPopupMenu;
 import javax.swing.KeyStroke;
 import javax.swing.SwingUtilities;
+import javax.swing.Timer;
 import javax.swing.event.PopupMenuEvent;
 import javax.swing.event.PopupMenuListener;
 
@@ -249,27 +258,8 @@ public class Menus {
         }
 
         // Install tray icon
-        if (Dialogs.instance().getAppImage() != null && SystemTray.isSupported()) {
-            PopupMenu popupMenu = new PopupMenu(Runtime.getAppName());
-            for (Menu menu : menuBar) {
-                java.awt.Menu target = new java.awt.Menu(menu.getText());
-                target.setEnabled(menu.isEnabled());
-                popupMenu.add(target);
-                addMenuItems(menu, target);
-            }
-            
-            if (systemTray == null) {
-                systemTray = SystemTray.getSystemTray();
-                trayIcon = new TrayIcon(Dialogs.instance().getAppImage(), Runtime.getAppName());
-                trayIcon.setImageAutoSize(true);
-                try {
-                    systemTray.add(trayIcon);
-                } catch (AWTException ex) {
-                    ex.printStackTrace();
-                }
-            }
-            trayIcon.setPopupMenu(popupMenu);
-        }
+//        installTrayIconAwt();
+        installTrayIconSwing();
 
         // Install context menu on main frame
         if (mainFrame == null)
@@ -280,7 +270,7 @@ public class Menus {
                 JMenu target = new JMenu(menu.getText());
                 target.setEnabled(menu.isEnabled());
                 popupMenu.add(target);
-                addMenuItems(menu, target);
+                addMenuItems(menu, target, null);
             }
             this.popupMenu = popupMenu;
             popupMenu.addPopupMenuListener(new PopupMenuListener() {
@@ -304,10 +294,160 @@ public class Menus {
         }
     }
 
-    private void addMenuItems(Menu menu, JMenu target) {
+	void installTrayIconAwt() {
+		if (Dialogs.instance().getAppImage() != null && SystemTray.isSupported()) {
+            PopupMenu popupMenu = new PopupMenu(Runtime.getAppName());
+            for (Menu menu : menuBar) {
+                java.awt.Menu target = new java.awt.Menu(menu.getText());
+                target.setEnabled(menu.isEnabled());
+                popupMenu.add(target);
+                addMenuItems(menu, target);
+            }
+            
+            if (systemTray == null) {
+                systemTray = SystemTray.getSystemTray();
+                trayIcon = new TrayIcon(Dialogs.instance().getAppImage(), Runtime.getAppName());
+                trayIcon.setImageAutoSize(true);
+                try {
+                    systemTray.add(trayIcon);
+                } catch (AWTException ex) {
+                    ex.printStackTrace();
+                }
+            }
+            trayIcon.setPopupMenu(popupMenu);
+        }
+	}
+	
+	private JPopupMenu trayPopupMenu;
+	private MouseListener trayMouseListener;
+	private Timer trayPopupMenuHider;
+	private int nbInsideTrayIconMenuItems;
+	
+
+	/**
+	 * Although it is possible to show a {@link JPopupMenu} (instead of a {@link PopupMenu}) when right-clicking on the
+	 * {@link TrayIcon}, the menu will not automatically disappear when clicking outside of it.
+	 * <p>
+	 * Hence, we use this {@link MouseAdapter}, that listen to mouse-enter and mouse-exit events on every menu items,
+	 * in order to check whether the mouse is over the popup menu or not. If the mouse is not over the popup menu
+	 * for more than 1 second, the popup menu is hidden.
+	 */
+	private class EnterExitAdapter extends MouseAdapter {
+		
+		@Override
+		public void mouseEntered(MouseEvent e) {
+			nbInsideTrayIconMenuItems++;
+			if (nbInsideTrayIconMenuItems > 0) {
+				disablePopupHider();
+			}
+		}
+
+		@Override
+		public void mouseExited(MouseEvent e) {
+			nbInsideTrayIconMenuItems--;
+			if (nbInsideTrayIconMenuItems <= 0) {
+	        	enablePopupHider();
+			}
+		}
+
+		private void enablePopupHider() {
+			disablePopupHider();
+			trayPopupMenuHider = new Timer((int) TimeUnit.SECONDS.toMillis(1), (ActionEvent et) -> {
+				if (trayPopupMenuHider != et.getSource())
+					return;
+				trayPopupMenu.setVisible(false);
+				trayPopupMenuHider = null;
+			});
+			trayPopupMenuHider.setRepeats(false);
+			trayPopupMenuHider.start();
+		}
+
+		private void disablePopupHider() {
+			if (trayPopupMenuHider != null) {
+				trayPopupMenuHider.stop();
+				trayPopupMenuHider = null;
+			}
+		}
+		
+	}
+	
+	/**
+	 * Install a {@link TrayIcon} with a swing popup menu ({@link JPopupMenu} instead of AWT's {@link PopupMenu}).
+	 * <p>
+	 * We need to listen to mouse events and to show the popup menu manually.
+	 * <p>
+	 * For some reason, AWT's {@link PopupMenu} sucks as it does not use the correct look&feel, it even seems
+	 * to use CDE Look&feel on Linux (!). Hopyfully, using {@link JPopupMenu} is better, despite of a few
+	 * caveats (see {@link EnterExitAdapter}).
+	 * TODO (3) in full screen mode, menu is too small because uiScale is set to 1
+	 */
+	void installTrayIconSwing() {
+		if (Dialogs.instance().getAppImage() != null && SystemTray.isSupported()) {
+			trayPopupMenu = new JPopupMenu(Runtime.getAppName());
+			for (Menu menu : menuBar) {
+                JMenu target = new JMenu(menu.getText());
+    			EnterExitAdapter activityAdapter = new EnterExitAdapter();
+                target.addMouseListener(activityAdapter);
+                target.setEnabled(menu.isEnabled());
+                trayPopupMenu.add(target);
+                addMenuItems(menu, target, (item) -> {
+                    item.addMouseListener(activityAdapter);
+                });
+			}
+
+            if (systemTray == null) {
+                systemTray = SystemTray.getSystemTray();
+                trayIcon = new TrayIcon(Dialogs.instance().getAppImage(), Runtime.getAppName());
+                trayIcon.setImageAutoSize(true);
+                try {
+                    systemTray.add(trayIcon);
+                } catch (AWTException ex) {
+                    ex.printStackTrace();
+                }
+                
+                if (trayMouseListener != null) {
+                	trayIcon.removeMouseListener(trayMouseListener);
+                	trayMouseListener = null;
+                }
+                
+                trayMouseListener = new MouseAdapter() {
+
+					@Override
+					public void mousePressed(MouseEvent e) {
+						maybeShowPopup(e);
+					}
+
+					@Override
+					public void mouseReleased(MouseEvent e) {
+						maybeShowPopup(e);
+					}
+                	
+					private void maybeShowPopup(MouseEvent e) {
+				        if (e.isPopupTrigger()) {
+				        	nbInsideTrayIconMenuItems = 0;
+				            GraphicsConfiguration graphicsConfiguration = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice().getDefaultConfiguration();
+				            AffineTransform screenTransform = graphicsConfiguration.getDefaultTransform();
+				            double scaleX = screenTransform.getScaleX();
+				            double scaleY = screenTransform.getScaleY();
+				            trayPopupMenu.setLocation((int) (e.getX() / scaleX) - 10, (int) (e.getY() / scaleY) - 10);
+				            trayPopupMenu.setInvoker(trayPopupMenu);
+				            trayPopupMenu.setVisible(true);
+				        }
+				    }
+				};
+				trayIcon.addMouseListener(trayMouseListener);
+            }
+		}
+	}
+
+    private void addMenuItems(Menu menu, JMenu target, Consumer<JComponent> onMenuItemCreated) {
         for (Menu item : menu.getItems()) {
             if (isSeparator(item)) {
-                target.addSeparator();
+            	JComponent separator = new JPopupMenu.Separator();
+                target.add(separator);
+                if (onMenuItemCreated != null) {
+                	onMenuItemCreated.accept(separator);
+                }
             } else {
                 JMenuItem menuItem;
                 if (item.getItems().isEmpty()) {
@@ -324,9 +464,12 @@ public class Menus {
                 if (item.isEnabled()) {
                     menuItem.addActionListener((e) -> menuSelected(item));
                 }
+                if (onMenuItemCreated != null) {
+                	onMenuItemCreated.accept(menuItem);
+                }
 
                 if (!item.getItems().isEmpty()) {
-                    addMenuItems(item, (JMenu) menuItem);
+                    addMenuItems(item, (JMenu) menuItem, onMenuItemCreated);
                 }
             }
         }
