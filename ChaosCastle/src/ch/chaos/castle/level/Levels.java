c1ef2b3763c5892c4f6d60c9471f278286e6858c
Nicolas Juillerat <tachyon.sonics@gmail.com>
2025-06-01 13:39:36 +0200

    dev

--- ChaosCastle/src/ch/chaos/castle/level/Levels.java
+++ ChaosCastle/src/ch/chaos/castle/level/Levels.java
@@ -29,9 +29,9 @@ public class Levels {
     public final static int SILENT_VOID = 23;
     public final static int SNOW_TRACK = 24;
     public final static int CITY_PARK = 25;
-    public final static int TWILIGHT = 26;
-    public final static int POND = 27;
-    public final static int BIG_CAVERN = 28;
+    public final static int BIG_CAVERN = 26;
+    public final static int TWILIGHT = 27;
+    public final static int POND = 28;
     public final static int INDUSTRY = 29;
     public final static int JUNKYARD = 30;
     
