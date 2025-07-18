d6d1daa7fe8b49432c5c674349ac414fd9925b52
Nicolas Juillerat <tachyon.sonics@gmail.com>
2025-07-18 14:40:51 +0200

    fixed similarity sound detection

--- Library/src/ch/chaos/library/sounds/ControlledWave.java
+++ Library/src/ch/chaos/library/sounds/ControlledWave.java
@@ -10,14 +10,24 @@ public record ControlledWave(long time, SoundWave wave, SoundControls controls) 
     public boolean isSimilarTo(ControlledWave other) {
         if (!this.wave.equals(other.wave))
             return false;
-        if (Math.abs(this.controls.getRate() - other.controls.getRate()) > 10.0)
+        if (!isSimilar(this.controls.getRate(), other.controls.getRate(), 10.0))
             return false;
-        if (Math.abs(this.controls.getStereo() - other.controls.getStereo()) > 10)
+        if (!isSimilar(this.controls.getStereo(), other.controls.getStereo(), 10.0))
             return false;
-        if (Math.abs(this.controls.getBalance() - other.controls.getBalance()) > 10)
+        if (!isSimilar(this.controls.getBalance(), other.controls.getBalance(), 10.0))
             return false;
-        if (Math.abs(this.controls.getVolume() - other.controls.getVolume()) > 10)
+        if (!isSimilar(this.controls.getVolume(), other.controls.getVolume(), 10.0))
             return false;
         return true;
+    }
+    
+    private boolean isSimilar(Number n1, Number n2, double threshold) {
+        if (n1 == null && n2 == null)
+            return true;
+        if (n1 == null || n2 == null)
+            return false;
+        if (Math.abs(n1.doubleValue() - n2.doubleValue()) <= threshold)
+            return true;
+        return false;
     }
 }
